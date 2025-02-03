42081fc7856addd2aa32663ee3b9ac1847d6a172
Ryan Lok <rlok.pc@gmail.com>
2025-02-03 12:43:34 -0800

    Added validation for click to upload

--- src/public/js/upload.js
+++ src/public/js/upload.js
@@ -14,32 +14,38 @@ fileDrop.addEventListener('dragleave', () => {
 
 fileDrop.addEventListener('drop', (e) => {
     e.preventDefault()
-    fileDrop.classList.remove('bg-light')
-
-    files = e.dataTransfer.files
-
-    if (files.length !== 1) {
-        alert("Please drop exactly one file.");
-        return;
-    }
-    const fileName = files[0].name
-    const fileExtension = fileName.split('.').pop().toLowerCase();
-
-    if (!fileExtension.includes('csv')) {
-        alert("Please attach .csv files only.");
-        return;
-    }
-
-    formFile.files = e.dataTransfer.files
-    updatefileDropText()
-    console.log(formFile.files)
+    validateFile(e.dataTransfer.files);
 })
 
-formFile.addEventListener('change', updatefileDropText)
+formFile.addEventListener('change', (e) => {
+    if (!validateFile(e.target.files)) {
+        e.target.value = ''
+    }
+})
 
 function updatefileDropText() {
     fileDrop.textContent =
         formFile.files.length > 0
             ? formFile.files[0].name
             : 'Drag and drop a file here or click to select'
+}
+
+function validateFile(files) {
+    fileDrop.classList.remove('bg-light')
+
+    if (files.length !== 1) {
+        alert("Please drop exactly one file.");
+        return false
+    }
+    const fileName = files[0].name
+    const fileExtension = fileName.split('.').pop().toLowerCase();
+
+    if (!fileExtension.includes('csv')) {
+        alert("Please attach .csv files only.");
+        return false
+    }
+
+    formFile.file = files[0]
+    updatefileDropText()
+    return true
 }
