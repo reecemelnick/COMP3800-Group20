1a5e9dfa3ab8398cbbb65a502d826552b531618c
LouiseYLi <louiseli.van@gmail.com>
2025-02-03 21:59:38 -0800

    added input validation for birthdate form field

--- src/public/js/predictDemographic.js
+++ src/public/js/predictDemographic.js
@@ -1,17 +1,44 @@
-let birthdate = document.getElementById('birthdate')
+let birthdate = document.getElementById('birthdate');
+let calculateBtn = document.getElementById('form-calculate');
 
 birthdate.addEventListener('change', (e) => {
     const birthdateVal = e.target.value;
-    document.getElementById('birthdateSelected').innerText = birthdateVal;
-
     const split_birthdate = birthdateVal.split("-");
     const year = parseInt(split_birthdate[0]);
     const month = parseInt(split_birthdate[1]);
     const day = parseInt(split_birthdate[2]);
 
+    if (!isValidBirthdate(year, month - 1, day)) {
+        document.getElementById('birthdateSelected').innerText = "Invalid birthdate.";
+        document.getElementById('birthdateSelected').style.color = "red";
+        calculateBtn.disabled = true;
+    } else {
+        document.getElementById('birthdateSelected').removeAttribute('style');
+        document.getElementById('birthdateSelected').innerText = birthdateVal;
+        calculateBtn.disabled = false;
+    }
+
 });
 
-let calculateBtn = document.getElementById('form-calculate');
+function isValidBirthdate(birthYear, birthMonth, birthDay) {
+    const today = new Date();
+    if (today.getFullYear() < birthYear) {
+        return false;
+    } else if (today.getFullYear() > birthYear) {
+        return true;
+    } else if (today.getMonth() > birthMonth) {
+        return true;
+    } else if (today.getMonth() < birthMonth) {
+        return false;
+    } else if (today.getDate() < birthDay) {
+        return false;
+    } else if (today.getDate() > birthDay) {
+        return true;
+    }
+
+    return false;
+}
+
 
 calculateBtn.addEventListener('click', (e) => {
     const probability = 75;
