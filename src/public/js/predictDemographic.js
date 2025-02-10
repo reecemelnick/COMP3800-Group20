05a20be5e51ef1896a72b165f87dbfefc655c90c
LouiseYLi <louiseli.van@gmail.com>
2025-02-10 14:11:11 -0800

    added eventlistener to each dropdown form to display the selected item

--- src/public/js/predictDemographic.js
+++ src/public/js/predictDemographic.js
@@ -39,10 +39,42 @@ function isValidBirthdate(birthYear, birthMonth, birthDay) {
     return false;
 }
 
-
 calculateBtn.addEventListener('click', (e) => {
     const probability = 75;
 
     document.getElementById('probabilityValue').innerText = probability;
 
 });
+
+function updateDropdownButtonText(dropdownButton, dropdownItems) {
+    dropdownItems.forEach(item => {
+        item.addEventListener('click', () => {
+            dropdownButton.textContent = item.textContent;
+        });
+    });
+}
+
+function setupDropdownEventListeners() {
+    updateDropdownButtonText(document.getElementById('dropdown-location'),
+        document.querySelectorAll('#form-field-location .dropdown-item'));
+    updateDropdownButtonText(document.getElementById('dropdown-socioeconomicstatus'),
+        document.querySelectorAll('#form-field-socioeconomicstatus .dropdown-item'));
+    updateDropdownButtonText(document.getElementById('dropdown-timeinlocation'),
+        document.querySelectorAll('#form-field-timeinlocation .dropdown-item'));
+    updateDropdownButtonText(document.getElementById('dropdown-lifestyle'),
+        document.querySelectorAll('#form-field-lifestyle .dropdown-item'));
+    updateDropdownButtonText(document.getElementById('dropdown-occupation'),
+        document.querySelectorAll('#form-field-occupation .dropdown-item'));
+    updateDropdownButtonText(document.getElementById('dropdown-diet'),
+        document.querySelectorAll('#form-field-diet .dropdown-item'));
+    updateDropdownButtonText(document.getElementById('dropdown-healthhabits'),
+        document.querySelectorAll('#form-field-healthhabits .dropdown-item'));
+    updateDropdownButtonText(document.getElementById('dropdown-healthconcerns'),
+        document.querySelectorAll('#form-field-healthconcerns .dropdown-item'));
+    updateDropdownButtonText(document.getElementById('dropdown-recallfrequency'),
+        document.querySelectorAll('#form-field-recallfrequency .dropdown-item'));
+    updateDropdownButtonText(document.getElementById('dropdown-referralsource'),
+        document.querySelectorAll('#form-field-referralsource .dropdown-item'));
+}
+
+setupDropdownEventListeners();
